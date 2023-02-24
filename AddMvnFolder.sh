a96053fd02883556f15076b863a63c32b398335f
MD Towhidul Islam <tuhintowhidul9@gmail.com>
2023-02-24 18:18:25 +0600

    update few things
    Took 2 hours 45 minutes

---
+++ AddMvnFolder.sh
@@ -0,0 +1,8 @@
+
+cp -Rf .mvn apigateway
+cp -Rf .mvn configserver
+cp -Rf .mvn orderservice
+cp -Rf .mvn paymentservice
+cp -Rf .mvn productservice
+cp -Rf .mvn service-registry
+cp -Rf .mvn spring-boot-oauth2-social-login
