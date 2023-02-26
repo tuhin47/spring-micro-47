0ba94f83676a4a371dc1e4d6c61e2b56e676e916
MD Towhidul Islam <tuhintowhidul9@gmail.com>
2023-02-26 08:45:24 +0600

    add dependencies for docker build
    Took 2 minutes

--- AddMvnFolder.sh
+++
@@ -1,8 +0,0 @@
-
-cp -Rf .mvn apigateway
-cp -Rf .mvn configserver
-cp -Rf .mvn orderservice
-cp -Rf .mvn paymentservice
-cp -Rf .mvn productservice
-cp -Rf .mvn service-registry
-cp -Rf .mvn spring-boot-oauth2-social-login
