b04672386995e19d4d9b860a1f2a3cbe6a80812a
MD Towhidul Islam <tuhintowhidul9@gmail.com>
2023-05-27 11:33:49 +0600

    zipkin in auth-service

--- runProjech.sh
+++ runProjech.sh
@@ -1,27 +1,7 @@
-## Clone Project
-```shell
-git clone https://github.com/tuhin47/spring-micro-47.git
-```
-
-## Installing docker-compose
-
-```shell
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
-```
-
-```shell
+git clone https://github.com/tuhin47/spring-micro-47.git
+cd spring-micro-47
 chmod +x **/mvnw
-```
-
-## Run docker file 
-```shell
 docker-compose --env-file .env -f docker-compose-prod.yml  up
-```
-
-## lazydocker
-
-```shell
-go install github.com/jesseduffield/lazydocker@latest
-```
