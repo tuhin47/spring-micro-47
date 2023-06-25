46c5fc1161deba4c59d1d562871394e7aa06b392
MD Towhidul Islam <tuhintowhidul9@gmail.com>
2023-06-25 10:32:04 +0600

    new version tag v0.1.2
    Took 17 minutes

---
+++ VersionUpdate.sh
@@ -0,0 +1,14 @@
+
+json=$(cat ./angular-boilerplate/package.json)
+previous_version=$(echo $json | jq -r '.version')
+echo "Previous Version : $previous_version"
+
+newVersion=$1
+
+if [ -z "$newVersion" ]; then
+    echo "Enter New Version:"
+    read newVersion
+fi
+
+npm -C ./angular-boilerplate version $newVersion
+mvn -B versions:set -DnewVersion=$newVersion -DgenerateBackupPoms=false
