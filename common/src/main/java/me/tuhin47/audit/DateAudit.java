ab55a4aeac275525b2421df72095a27d4885e6e9
MD Towhidul Islam <tuhintowhidul9@gmail.com>
2023-06-25 15:04:15 +0600

    code clean up
    Took 12 minutes

--- common/src/main/java/me/tuhin47/audit/DateAudit.java
+++ common/src/main/java/me/tuhin47/audit/DateAudit.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
