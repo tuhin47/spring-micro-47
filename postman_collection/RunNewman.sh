0256f6d66162f70fcba7229e27a3c758598cc8ab
MD Towhidul Islam <tuhintowhidul9@gmail.com>
2023-09-19 23:36:37 +0600

    update testservice + testing enabled on ci

--- postman_collection/RunNewman.sh
+++ postman_collection/RunNewman.sh
@@ -14,4 +14,4 @@ onExit() {
 
 trap onExit EXIT
 # --bail
-newman run Microservice_Collection.postman_collection.json --delay-request=500 --environment=env.docker.json --reporters=cli,junit --iteration-count=2
+newman run Microservice_Collection.postman_collection.json --environment=env.docker.json --reporters=cli,junit --iteration-count=2
