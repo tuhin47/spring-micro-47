72054961c9bab803c50e02e833f4efd835263f72
MD Towhidul Islam <tuhintowhidul9@gmail.com>
2023-09-24 20:40:30 +0600

    [FE] update svg on error pages

--- angular-boilerplate/src/app/static/access-denied/access-denied.component.ts
+++ angular-boilerplate/src/app/static/access-denied/access-denied.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'access-denied',
     templateUrl: './access-denied.component.html',
-  styleUrls   : ['../not-found/not-found.component.scss']
 
 })
 export class AccessDeniedComponent {
