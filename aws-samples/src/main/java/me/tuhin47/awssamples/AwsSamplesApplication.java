ef209f6d3523a395e60f74a406e4589f69d7f048
MD Towhidul Islam <tuhintowhidul9@gmail.com>
2024-04-05 12:10:55 +0600

    init new module for aws

---
+++ aws-samples/src/main/java/me/tuhin47/awssamples/AwsSamplesApplication.java
@@ -0,0 +1,13 @@
+package me.tuhin47.awssamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsSamplesApplication.class, args);
+    }
+
+}
---
+++ aws-samples/src/test/java/me/tuhin47/awssamples/AwsSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tuhin47.awssamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
