023da8c1e1b09c5a6c75726820c17718612cdf5e
MD Towhidul Islam <tuhintowhidul9@gmail.com>
2025-01-03 23:07:05 +0600

    [Spring3] v0.7.0

--- VersionUpdate.sh
+++ VersionUpdate.sh
@@ -11,11 +11,10 @@ if [ -z "$newVersion" ]; then
 fi
 
 npm -C ./angular-boilerplate version $newVersion
-/opt/apache-maven-3.6.0/bin/mvn -B versions:set -DnewVersion=$newVersion -DgenerateBackupPoms=false
+/home/towhidul/.local/share/JetBrains/Toolbox/apps/intellij-idea-ultimate/plugins/maven/lib/maven3/bin/mvn -B versions:set -DnewVersion=$newVersion -DgenerateBackupPoms=false
 
 sed -i "s/^IMAGE_TAG=.*/IMAGE_TAG=$newVersion/" .env*
 sed -i "s/$previous_version/$newVersion/" k8s/*.yaml
 sed -i "s/<IMAGE_TAG>$previous_version<\/IMAGE_TAG>/<IMAGE_TAG>$newVersion<\/IMAGE_TAG>/" pom.xml
 
-export JAVA_HOME=$HOME/.jdks/corretto-17.0.10
-/opt/apache-maven-3.6.0/bin/mvn clean install
+/home/towhidul/.local/share/JetBrains/Toolbox/apps/intellij-idea-ultimate/plugins/maven/lib/maven3/bin/mvn clean install
